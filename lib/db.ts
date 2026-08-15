88d2075960601de8dd181fa3063077779704f7c4
Rayyan Arif <rayyanarif114@gmail.com>
2026-08-15 16:24:48 +0500

    db connected

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,17 @@
 import { Pool } from "pg";
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: +process.env.DB_PORT!
-});
+const pool = new Pool(
+    process.env.NODE_ENV === 'development' ? 
+    {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USERNAME,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: +process.env.DB_PORT!
+    } : 
+    {
+        connectionString: process.env.DB_URL,
+    }
+);
 
 export default pool;
